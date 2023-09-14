9a14e0420f64015a3ff0bdc2c44d926f93a59a7c
Daniel G <80426387+DrSpaniel@users.noreply.github.com>
2023-09-14 16:47:45 -0400

    ok done alien

--- experiments/make an alien/js/script.js
+++ experiments/make an alien/js/script.js
@@ -30,13 +30,23 @@ function setup() {
   noStroke();
 
   fill(255, 255, 255);
-  ellipse(240, 160, 70, 70); //coordinates of circle
+  ellipse(240, 160, 70, 70); //coordinates of left eye
+
+  fill(0, 0, 0);
+  ellipse(240, 160, 40, 40); //pupil
 
   fill(255, 255, 255);
-  ellipse(400, 160, 70, 70); //coordinates of circle
+  ellipse(400, 160, 70, 70); //coordinates of right eye
 
-  fill(255, 255, 255);
-  ellipse(320, 370, 90, 110); //coordinates of circle
+  fill(0, 0, 0);
+  ellipse(400, 160, 40, 40);
+
+  fill(0, 230, 0);
+  ellipse(320, 370, 90, 110); //coordinates of mouth!
+
+  fill(200, 0, 0);
+  ellipse(320, 370, 70, 90); //coordinates of mouth!
+
 }
 
 /**
