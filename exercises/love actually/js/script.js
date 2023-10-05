f8aea28c5f8fa2996a770b4322cde13de4ab0571
Daniel G <80426387+DrSpaniel@users.noreply.github.com>
2023-10-05 15:28:33 -0400

    working on looking for love, not done yet

--- exercises/love actually/js/script.js
+++ exercises/love actually/js/script.js
@@ -1,32 +1,93 @@
 /**
- * Title of Project
- * Author Name
- * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+looking for love project
  */
 
 "use strict";
 
 /**
  * Description of preload
-*/
-function preload() {
+ */
+function preload() {}
+//global variables
 
-}
+let circle1 = {
+  //player obj
+  x: undefined,
+  y: undefined,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 3,
+};
 
+let circle2 = {
+  //random obj
+  x: undefined,
+  y: undefined,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 3,
+};
 
 /**
  * Description of setup
-*/
+ */
 function setup() {
+  createCanvas(600, 600);
 
+  circle2.vx = random(-circle2.speed, circle2.speed);
+  circle2.vy = random(-circle2.speed, circle2.speed);
+
+  circle2.x = width / 2;
+  circle2.y = width / 2;
 }
 
+//todo:
+//position circle2
+//set circle1 to a delayed mouse position
+//set circle2 to move random direction
+//check if circles overlap
+//if they do, both circles morph into a heart
+//make arrow keys change size of circle1
 
 /**
  * Description of draw()
-*/
+ */
 function draw() {
+  background(0);
 
+  
+
+  //move circle2
+  circle2.x = circle2.x + circle2.vx;
+  circle2.y = circle2.y + circle2.vy;
+
+  //check if circles overlap
+  let d = dist(circle1.x, circle1.y, circle2.x, circle2.y);
+  if (d < circle1.size / 2 + circle2.size / 2) {
+    //if they do, morph into heart
+    fill(255, 100, 100);
+    ellipse(width / 2, height / 2, 100, 100);
+  }
+
+  //display circles
+  ellipse(circle1.x, circle1.y, circle1.size);
+  ellipse(circle2.x, circle2.y, circle2.size);
+
+  //offscreenCheck();
+}
+
+offscreenCheck();
+{
+  //check if circles have gone offscreen
+  if (
+   // circle2.x < 0 ||
+    circle2.x > width ||
+    //circle2.y < 0 ||
+    circle2.y > height
+  ) {
+    //if they have, reset circles
+    circle2.x = width / 2;
+  }
 }
--- experiments/looking for love/js/script.js
+++ experiments/looking for love/js/script.js
@@ -13,7 +13,53 @@
  */
 function preload() {}
 
-//globals
+/**
+ * Description of setup
+ */
+function setup() {
+  createCanvas(600, 600);
+
+  circleSetup();
+}
+
+/**
+ * Description of draw()
+ */
+function draw() {
+  if (state === "title") {
+    title();
+  } else if (state === "simulation") {
+    simulate();
+  } else if (state === "love") {
+    love();
+  } else if (state === "sadness") {
+    sadness();
+  }
+}
+
+function simulate() {
+  background(0);
+  move();
+  checkOffScreen();
+  checkOverlap();
+  display();
+}
+
+function title() {
+  push();
+  textSize(64);
+  fill(200, 100, 100);
+  textAlign(CENTER, CENTER);
+  text("LOVE SIMULATOR", width / 2, height / 2);
+  pop();
+}
+
+function love() {}
+
+function sadness() {}
+
+/*GLOBAL VARIABLES*/
+
 let circle1 = {
   x: undefined,
   y: 200,
@@ -32,15 +78,17 @@ let circle2 = {
   speed: 3,
 };
 
-/**
- * Description of setup
- */
-function setup() {
-  createCanvas(600, 600);
+let state = "title"; //can be title, simulation, love, sadness
 
+/*FUNCTIONS*/
+
+function circleSetup() {
   //position circles seperately
-  circle1.x = width / 3;
-  circle2.x = width / 2;
+  circle1.x = random(0, width / 2);
+  circle2.x = random(width / 2, width);
+
+  circle1.y = random(0, width / 2);
+  circle2.y = random(width / 2, width);
 
   //start circles moving in a random direction
   circle1.vx = random(-circle1.speed, circle1.speed);
@@ -49,44 +97,49 @@ function setup() {
   circle2.vy = random(-circle2.speed, circle2.speed);
 }
 
-/**
- * Description of draw()
- */
-function draw() {
-  background(0);
-
+function move() {
   //move x and y of circles
   circle1.x = circle1.x + circle1.vx;
   circle1.y = circle1.y + circle1.vy;
 
   circle2.x = circle2.x + circle2.vx;
   circle2.y = circle2.y + circle2.vy;
+}
 
+function checkOffScreen() {
   //check if circles have gone offscreen
   if (
     circle1.x < 0 ||
     circle1.x > width ||
     circle1.y < 0 ||
     circle1.y < height ||
-
     circle2.x < 0 ||
     circle2.x > width ||
     circle2.y < 0 ||
     circle2.y > height
   ) {
     //SAD ENDING
-
   }
+}
 
+function checkOverlap() {
   //check if circles overlap
 
   let d = dist(circle1.x, circle1.y, circle2.x, circle2.y);
 
   if (d < circle1.size / 2 + circle2.size / 2) {
     //HAPPY ENDING
-    noLoop();
+    //noLoop();
   }
+}
 
+function display() {
   ellipse(circle1.x, circle1.y, circle1.size);
   ellipse(circle2.x, circle2.y, circle2.size);
+}
+
+function mousePressed() {
+  if (state === "title") {
+    state = "simulation";
+  }
 }
