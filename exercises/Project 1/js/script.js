0419f394f96234d9b5028b95320766fb9f1edd64
Daniel G <80426387+DrSpaniel@users.noreply.github.com>
2023-10-17 16:32:25 -0400

    changelog below
    done:
    - redid structure of meteor
    - turned it into a class, to allow multiple meteors
    - some other funky fixes
    - added shitty title and end
    
    todo:
    - proper end screen
    - implement leaderboard
    - add incremental speed and frequency of meteors
    - add gif of explosion when dead.
    - change background to space. crude drawing

--- exercises/Project 1/js/script.js
+++ exercises/Project 1/js/script.js
@@ -1,118 +1,158 @@
-"use strict";
+let scene = "title"; // Initial scene is set to "title"
 
-let meteors = []; //array to store meteor objects
+class Meteor {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.img = 0;
+    this.vx = 0;
+    this.vy = 0;
+    this.initialVx = 0;
+    this.initialVy = 0;
+    this.speed = 3;
+    this.meteorPos = 0;
+    this.randPosX = [
+      // Predetermined locations for meteor.x
+      windowWidth / 4,
+      windowWidth / 2,
+      (3 * windowWidth) / 4,
+      windowWidth,
+      windowWidth,
+      windowWidth,
+      (3 * windowWidth) / 4,
+      windowWidth / 2,
+      windowWidth / 4,
+      0,
+      0,
+      0,
+    ];
+    this.randPosY = [
+      // Meteor.y preset coordinates
+      0,
+      0,
+      0,
+      height / 4,
+      height / 2,
+      (3 * height) / 4,
+      height,
+      height,
+      height,
+      (3 * height) / 4,
+      height / 2,
+      height / 4,
+    ];
+    this.img = loadImage("assets/images/meteor.png");
+    this.spawnMeteor();
+  }
 
-let meteor = {
-  x: 0,
-  y: 0,
-  img: 0,
-  diff: 0,
-  vx: 0,
-  vy: 0,
-  initialVx: 0,
-  initialVy: 0,
-  speed: 3,
-  meteorPos: 0,
-  randPosX: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  randPosY: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-};
+  move() {
+    // Update the meteor's position based on its velocity.
+    this.x += this.vx;
+    this.y += this.vy;
+  }
 
-function preload() {
-  meteor.img = loadImage("assets/images/meteor.png");
+  calculateDirection() {
+    // Calculate the direction vector from the meteor to the mouse's last location.
+    const dx = mouseX - this.x;
+    const dy = mouseY - this.y;
+    const magnitude = dist(mouseX, mouseY, this.x, this.y);
+
+    // Normalize the direction vector to get a unit vector.
+    if (this.vx === 0 && this.vy === 0) {
+      //only when the meteor is spawned will it change.
+      this.initialVx = (dx / magnitude) * this.speed;
+      this.initialVy = (dy / magnitude) * this.speed;
+    }
+    this.vx = this.initialVx;
+    this.vy = this.initialVy;
+  }
+
+  spawnMeteor() {
+    this.meteorPos = Math.floor(random(0, 11));
+    this.x = this.randPosX[this.meteorPos]; //sets predetermined locations for meteor
+    this.y = this.randPosY[this.meteorPos];
+    this.initialVx = 0;
+    this.initialVy = 0;
+    print("reset!"); //debug
+    this.calculateDirection();
+  }
 }
+
+let meteors = []; // Array to hold meteor objects
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(60);
   mouseX = width / 2;
   mouseY = height / 2;
-  meteor.randPosX = [
-    //this mess is all the predetermined locations the meteor.x can be in.
-    windowWidth / 4,
-    windowWidth / 2,
-    (3 * windowWidth) / 4,
-    windowWidth,
-    windowWidth,
-    windowWidth,
-    (3 * windowWidth) / 4,
-    windowWidth / 2,
-    windowWidth / 4,
-    0,
-    0,
-    0,
-  ];
+  meteor = new Meteor(); // Create the initial meteor object
+}
 
-  meteor.randPosY = [
-    //this mess is the meteor.y preset coordinates. in the meteor spawn the meteorPos is randomized between all these.
-    0,
-    0,
-    0,
-    height / 4,
-    height / 2,
-    (3 * height) / 4,
-    height,
-    height,
-    height,
-    (3 * height) / 4,
-    height / 2,
-    height / 4,
-  ];
+function spawnNewMeteor() {
+  meteors.push(new Meteor()); // Create a new meteor and add it to the array
+}
 
-  spawnMeteor();
+function draw() {
+  if (scene === "title") {
+    // Title screen
+    background(0, 34, 88); // Set the background color to dark blue (RGB values).
+    fill(255); // Set the fill color to white
+    textAlign(CENTER, CENTER);
+    textSize(60);
+    text("Space!", width / 2, height / 4);
+    textSize(29);
+    text("click to start.", width / 2, (3 * height) / 4);
+    textSize(8);
+    text("drspaniel.com", width / 8, (7 * height) / 8); //shameless plug
+
+    if (mouseIsPressed) {
+      // If the mouse is clicked, transition to the simulation scene
+      scene = "simulation";
+      startMeteorSpawnInterval(); // Start spawning meteors
+    }
+  } else if (scene === "simulation") {
+    background(0, 34, 88); // Set the background color to dark blue (RGB values).
+
+    for (let i = meteors.length - 1; i >= 0; i--) {
+      const meteor = meteors[i]; // Get the current meteor object from the array.
+
+      image(meteor.img, meteor.x - 32, meteor.y - 32); // Display the meteor's image at its current position with an offset to center it.
+
+      meteor.calculateDirection(); // Calculate the direction of the meteor's movement based on the mouse position.
+
+      meteor.move(); // Update the meteor's position based on its velocity.
+
+      if (
+        meteor.y > height ||
+        meteor.y < 0 ||
+        meteor.x > width ||
+        meteor.x < 0
+      ) {
+        meteors.splice(i, 1); // Remove meteors that go off-screen from the array.
+      }
+      if (dist(meteor.x, meteor.y, mouseX, mouseY) < 55) {  //64 to account of radius of both meteor and ship. probably janky.
+        // If the mouse touches a meteor, transition to the "end" scene
+        scene = "end";
+        clearInterval(meteorSpawnInterval); // Stop spawning meteors
+      }
+    }
+  } else if (scene === "end") {
+    background(0, 34, 88); // Set the background color to dark blue (RGB values).
+    fill(255); // Set the fill color to white
+    textAlign(CENTER, CENTER);
+    textSize(48);
+    text("DEAD.", width / 2, height / 2);
+ 
+  } else if (scene === "board") {   //WIP, this will be a leaderboard to submit results. title page will have a list
+
+    background(0); // Set the background color to black
+  }
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-function draw() {
-  //meteor.diff = dist(meteor.x, meteor.y, mouseX, mouseY);
-  background(0, 34, 88); //plain colour for now
-
-  image(meteor.img, meteor.x - 32, meteor.y - 32); //centering image so that tracking looks believable.
-
-  calculateMeteorDirection(); // Call this function in draw to calculate direction
-
-  moveMeteor();
-  wallCheck();
-}
-
-function wallCheck() {
-  // Check if the meteor touches any border
-  if (meteor.y > height || meteor.y < 0 || meteor.x > width || meteor.x < 0) {
-    spawnMeteor();
-  }
-}
-
-function moveMeteor() {
-  // Update the meteor's position based on its velocity.
-  meteor.x += meteor.vx;
-  meteor.y += meteor.vy;
-}
-
-function calculateMeteorDirection() {
-  // Calculate the direction vector from the meteor to the mouse's last location.
-  const dx = mouseX - meteor.x; //first calculate x and y coord distance..
-  const dy = mouseY - meteor.y;
-  const magnitude = sqrt(dx * dx + dy * dy); //then, using pythagoras, find the direct distance between meteor and mouse. linear algebra FUCK YEAH!
-
-  if (meteor.vx === 0 && meteor.vy === 0) {
-    //this can only happen when spawnmeteor is triggered
-    meteor.initialVx = (dx / magnitude) * meteor.speed; //this will then make vx and vy follow said line that was calculated above.
-    meteor.initialVy = (dy / magnitude) * meteor.speed;
-  }
-  meteor.vx = meteor.initialVx; //this runs in draw, meaning that vx and vy will only change when the above is true.
-  meteor.vy = meteor.initialVy;
-}
-
-function spawnMeteor() {
-  //meteor.speed++;
-  meteor.meteorPos = Math.floor(random(0, 12));
-  meteor.x = meteor.randPosX[meteor.meteorPos]; // Start at the center of the canvas
-  meteor.y = meteor.randPosY[meteor.meteorPos];
-  meteor.initialVx = 0; // Reset initialVX and initialVY when spawning a new meteor
-  meteor.initialVy = 0;
-  print("reset!");
-  print(meteor.meteorPos);
-  calculateMeteorDirection();
+function startMeteorSpawnInterval() {
+  meteorSpawnInterval = setInterval(spawnNewMeteor, 200); // Start spawning meteors
 }
