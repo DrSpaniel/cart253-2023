769ca5b568434dc4de3f22585cd588138fe13689
Daniel G <80426387+DrSpaniel@users.noreply.github.com>
2023-11-02 15:08:39 -0400

    got the base working.. now todo
    todo:
    
    add other user control
    (click to start ball stream? click for powerups, or spawn random blocks)
    
    add new class or object
    (this can be the new blocks or powerups)
    
    add 2 endings
    (one to survive the timer, another when you lose

--- Template/js/script.js
+++ Template/js/script.js
@@ -1,8 +1,8 @@
 /**
  * Title of Project
  * Author Name
- * 
- * This is a template. You must fill in the title, author, 
+ *
+ * This is a template. You must fill in the title, author,
  * and this description to match your project!
  */
 
@@ -10,23 +10,27 @@
 
 /**
  * Description of preload
-*/
-function preload() {
+ */
+function preload() {}
 
-}
-
+let bg;
 
 /**
  * Description of setup
-*/
+ */
 function setup() {
-
+  createCanvas(windowWidth, windowHeight); //autosize
+  bg = loadImage("assets/images/bg.jpg");
 }
-
 
 /**
  * Description of draw()
-*/
+ */
 function draw() {
+  background(bg);
+}
 
+function windowResized() {
+  //resize canvas when window is resized
+  resizeCanvas(windowWidth, windowHeight);
 }
