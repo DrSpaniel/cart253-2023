5c1b6d876de8106b9def632a14f42582c622b914
Daniel G <80426387+DrSpaniel@users.noreply.github.com>
2023-11-09 15:59:26 -0500

    first scene done!!!!!!!!!!!!
    a little laggy, but wtv.
    
    todo:
    when both are in basket, change to next scene

--- exercises/project2/js/script.js
+++ exercises/project2/js/script.js
@@ -15,11 +15,11 @@ function preload() {}
 
 //image declarations
 let bg;
-
 let s1;
 
 let felt;
 let pins;
+let placeSound;
 
 class Item {
   constructor(x, y, img) {
@@ -39,9 +39,10 @@ function setup() {
   createCanvas(heightCheck(), heightCheck()); //autosize
   bg = loadImage("assets/images/bg.jpg");
   s1 = loadImage("assets/images/s1.png");
+  placeSound = loadSound("assets/sounds/place.wav");
 
   felt = new Item(
-    width - width / 6 - 25, //x
+    width - width / 6 - 25, //x)
     height / 2 - 25, //y
     loadImage("assets/images/felt.png") //img
   );
@@ -53,6 +54,7 @@ function setup() {
 }
 
 function heightCheck() {
+  //make sure the square fits in the window
   if (windowHeight > windowWidth) {
     return windowWidth;
   } else {
@@ -63,10 +65,6 @@ function heightCheck() {
 function windowResized() {
   //resize canvas when window is resized
   resizeCanvas(heightCheck(), heightCheck());
-}
-
-function imageScale() {
-  //this will take the pixel width of the canvas, and scale
 }
 
 /**
@@ -109,11 +107,52 @@ function scene1() {
   image(pins.img, pins.x, pins.y); //pins on left shelf
 
   if (mouseIsPressed) {
+    if (
+      //if mouse is in felt area
+      mouseX > felt.x &&
+      mouseX < felt.x + felt.img.width &&
+      mouseY > felt.y &&
+      mouseY < felt.y + felt.img.height
+    ) {
+      felt.x = mouseX - felt.img.width / 2;
+      felt.y = mouseY - felt.img.height / 2;
+    } else if (
+      mouseX > pins.x &&
+      mouseX < pins.x + pins.img.width &&
+      mouseY > pins.y &&
+      mouseY < pins.y + pins.img.height
+    ) {
+      pins.x = mouseX - pins.img.width / 2;
+      pins.y = mouseY - pins.img.height / 2;
+    }
+
     //print mouseX and mouseY
     print("mouseX: ");
     print(mouseX);
     print("mouseY: ");
     print(mouseY);
+  }
+
+  if (
+    //if felt is within the basket area, dissapear
+    felt.x > 201 &&
+    felt.x < 355 &&
+    felt.y > 391 &&
+    felt.y < 490
+    //or if pins are within the basket area, dissapear
+  ) {
+    placeSound.play();
+    felt.x = -100;
+    felt.y = -100;
+  }else if(
+    pins.x > 201 &&
+    pins.x < 355 &&
+    pins.y > 391 &&
+    pins.y < 490
+  ){
+    placeSound.play();
+    pins.x = -100;
+    pins.y = -100;
   }
 }
 
